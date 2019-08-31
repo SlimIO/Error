a45bdcc9def10f5042a6c8ed9ab4606310e17ebe
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-08-31 19:00:31 +0200

    chore: draft 1.0 prototype

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,18 @@
-/// <reference types="@types/node" />
-/// <reference types="@types/es6-shim" />
 
-/**
- * Error class definition
- */
-declare class ErrorManager {
-    constructor(filePath: string);
-    static mapFromPayload(payload: ErrorManager.Error[]): Map<string, ErrorManager.Error>;
-
-    errorFile: string;
-    private _errors: Map<string, ErrorManager.Error>;
-    readonly errors: Set<string>;
-    readonly isInitialized: boolean;
-
-    public load(): Promise<void>;
-    public loadSync(): void;
-    public get(errorTitle: string): ErrorMessage;
-}
-
-/**
- * Error namespace
- */
-declare namespace ErrorManager {
-
-    export declare class ErrorMessage {
-
-    }
-
-    export enum criticity {
-        Critical,
-        Major,
-        Minor,
-        Debug
-    }
-
-    export interface Error {
-        title: string;
-        code: string;
-        handler: (args: string[] | { [k: string]: any }) => string;
-        description?: string;
+export declare class SlimIOError extends Error {
+    toJSON(): {
+        name: string;
         message: string;
-        criticity: ErrorManager.criticity
+        stack: string;
     }
-
 }
 
-export as namespace ErrorManager;
-export = ErrorManager;
+export declare class CallbackArgumentType extends SlimIOError {}
+export declare class CallbackNotFound extends SlimIOError {
+    constructor(callbackName: string);
+}
+export declare class RequiredArgument extends SlimIOError {
+    constructor(argName: string);
+}
+
+export function isSlimIOError(obj: any): boolean;
--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 "use strict";
 
+// Require Third-party Dependencies
+const cleanStack = require("clean-stack");
 
+// CONSTANTS
+const kError = Symbol.for("SlimIOError");
+
+class SlimIOError extends Error {
+    constructor(message) {
+        super(message);
+        Object.defineProperty(this, kError, { value: true });
+        this.stack = cleanStack(this.stack);
+    }
+
+    toJSON() {
+        return {
+            name: `${kError.description}::${this.name}`,
+            message: this.message,
+            stack: this.stack
+        };
+    }
+}
+
+class CallbackArgumentType extends SlimIOError {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidCallbackArgument";
+    }
+}
+
+class CallbackNotFound extends SlimIOError {
+    constructor(callbackName) {
+        super(`Unable to found callback with name '${callbackName}'`);
+        this.name = "CallbackNotFound";
+    }
+}
+
+class RequiredArgument extends SlimIOError {
+    constructor(argName) {
+        super(`Argument ${argName} is required`);
+        this.name = "RequiredArgument";
+    }
+}
+
+/**
+ * @function isSlimIOError
+ * @description check and tell if a given object is a SlimIO Error or not!
+ * @param {*} obj
+ * @returns {boolean}
+ */
+function isSlimIOError(obj) {
+    if (typeof obj !== "object" || obj === null) {
+        return false;
+    }
+    if (obj[kError]) {
+        return true;
+    }
+
+    return "name" in obj && typeof obj.name === "string" && obj.name.startsWith(kError.description);
+}
+
+module.exports = {
+    isSlimIOError,
+    SlimIOError,
+    CallbackArgumentType,
+    CallbackNotFound,
+    RequiredArgument
+};
